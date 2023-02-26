d21ab836e6e04cba064e0c7344f11f2b9d8d5e9d
TallFly <tallfly@vk.com>
2023-02-26 11:46:26 +0600

    пчиолы

--- t4llfly.github.io/src/App.tsx
+++ t4llfly.github.io/src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Text } from "@chakra-ui/react"
+import { Box, Button, ButtonGroup } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+    display='flex'
+    alignItems='center'
+    flexWrap="wrap"
+    width="100%"
+    bgColor="#222222"
+    py="432.5"
+    mb="0"
+    bgRepeat="no-repeat"
+    justifyContent="center"
+    >
+      <Text fontSize={64} fontFamily="sans-serif" letterSpacing="wide" fontWeight="800" textColor="white">TALLFLY</Text>
+      <ButtonGroup variant='solid' spacing='3'>
+        <Button colorScheme='telegram'>Telegram</Button>
+        <Button colorScheme='twitter'>Twitter</Button>
+      </ButtonGroup>
+    </Box>
   );
 }
 
