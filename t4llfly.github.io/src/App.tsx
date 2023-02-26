69e0f089c28d8f7de49889dad8959b7365133b6a
TallFly <tallfly@vk.com>
2023-02-26 14:28:08 +0600

    дополнил ссылки

--- t4llfly.github.io/src/App.tsx
+++ t4llfly.github.io/src/App.tsx
@@ -18,11 +18,35 @@ function App() {
     >
       <Text fontSize={64} fontFamily="sans-serif" letterSpacing="wide" fontWeight="800" textColor="white">TALLFLY</Text>
       <ButtonGroup variant='solid' spacing='3'>
-        <Button colorScheme='telegram'>Telegram</Button>
-        <Button colorScheme='twitter'>Twitter</Button>
+          <Button colorScheme='telegram' onClick={Telegram}>Telegram</Button>
+          <Button colorScheme="messenger" onClick={VK}>VK</Button>
+          <Button colorScheme='whiteAlpha' onClick={GitHub}>GitHub</Button>
+          <Button colorScheme="orange" onClick={Catangens}>Catangens</Button>
+          <Button colorScheme="blackAlpha" onClick={Rickroll}>TOP SECRET</Button>
       </ButtonGroup>
     </Box>
+
   );
+}
+
+function Telegram() {
+    window.open("https://t.me/t4llfly")
+}
+
+function GitHub() {
+  window.open("https://github.com/T4llFly")
+}
+
+function Catangens() {
+  window.open("https://catangens.ru")
+}
+
+function VK() {
+  window.open("https://vk.com/tallfly")
+}
+
+function Rickroll() {
+  window.open("https://youtu.be/dQw4w9WgXcQ")
 }
 
 export default App;
