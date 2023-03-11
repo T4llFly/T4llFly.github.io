3abbbc5ed115988066b13355451731cef110198b
TallFly <tallfly@vk.com>
2023-03-11 15:25:20 +0600

    added adaption for mobile

--- t4llfly.github.io/src/App.tsx
+++ t4llfly.github.io/src/App.tsx
@@ -1,31 +1,35 @@
 import './App.css';
 import { Text } from "@chakra-ui/react"
-import { Box, Button, ButtonGroup } from '@chakra-ui/react'
+import { Flex, Stack, Button, IconButton } from '@chakra-ui/react'
 import { FaGithub, FaCat, FaVk, FaTelegramPlane, FaEyeSlash } from 'react-icons/fa'
 
 function App() {
   return (
-    <Box
-    display='flex'
+    <Flex
     alignItems='center'
-    flexWrap="wrap"
-    width="100%"
-    bgColor="#222222"
-    py="412.5"
-    mb="0"
-    bgRepeat="no-repeat"
+    bg="#222222"
+    color="white"
+    minH="100vh"
+    p="4"
     justifyContent="center"
     flexDirection="column"
     >
-      <Text fontSize={64} fontFamily="sans-serif" letterSpacing="wide" fontWeight="800" textColor="white">TALLFLY</Text>
-      <ButtonGroup variant='solid' spacing='3'>
-          <Button colorScheme='telegram' leftIcon={<FaTelegramPlane />}onClick={Telegram}>Telegram</Button>
-          <Button colorScheme="messenger" leftIcon={<FaVk />} onClick={VK}>VK</Button>
-          <Button colorScheme='whiteAlpha' leftIcon={<FaGithub />} onClick={GitHub}>GitHub</Button>
-          <Button colorScheme="orange" leftIcon={<FaCat />} onClick={Catangens}>Catangens</Button>
-          <Button colorScheme="blackAlpha" leftIcon={<FaEyeSlash />} onClick={Rickroll}>TOP SECRET</Button>
-      </ButtonGroup>
-    </Box>
+      <Text fontSize={["4xl", "5xl", "6xl"]} fontFamily="sans-serif" letterSpacing="wide" fontWeight="800" mb="1">TALLFLY</Text>
+      <Stack spacing="4" direction={["row", "row"]} justify={["center", "space-between"]} align={["center", "flex-end"]}>
+          <Button colorScheme='telegram' leftIcon={<FaTelegramPlane />}onClick={Telegram} display={["none", "flex"]}>Telegram</Button>
+          <Button colorScheme="messenger" leftIcon={<FaVk />} onClick={VK} display={["none", "flex"]}>VK</Button>
+          <Button colorScheme='whiteAlpha' leftIcon={<FaGithub />} onClick={GitHub} display={["none", "flex"]}>GitHub</Button>
+          <Button colorScheme="orange" leftIcon={<FaCat />} onClick={Catangens} display={["none", "flex"]}>Catangens</Button>
+          <Button colorScheme="blackAlpha" leftIcon={<FaEyeSlash />} onClick={Rickroll} display={["none", "flex"]}>TOP SECRET</Button>
+          
+          <IconButton colorScheme='telegram' aria-label='Telegram' icon={<FaTelegramPlane />} onClick={Telegram} display={["flex", "none"]}/>
+          <IconButton colorScheme='messenger' aria-label='VK' icon={<FaVk />} onClick={VK} display={["flex", "none"]}/>
+          <IconButton colorScheme='whiteAlpha' aria-label='GitHub' icon={<FaGithub />} onClick={GitHub} display={["flex", "none"]}/>
+          <IconButton colorScheme='orange' aria-label='Catangens' icon={<FaCat />} onClick={Catangens} display={["flex", "none"]}/>
+          <IconButton colorScheme='blackAlpha' aria-label='TOP SECRET' icon={<FaEyeSlash />} onClick={Rickroll} display={["flex", "none"]}
+        />
+      </Stack>
+    </Flex>
 
   );
 }
